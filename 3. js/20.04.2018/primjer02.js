18e2abc849f4743eb888b4c172a5b74f2b5cc883
Edin Jašarević <edinjasar14@gmail.com>
2018-06-10 00:07:56 +0200

    dodavanje property

---
+++ 3. js/20.04.2018/primjer02.js
@@ -0,0 +1,6 @@
+var kuca = {};
+kuca.brojKatova = 3;
+kuca.brojKvadrata = 75;
+kuca.bojaFasade = "zuta";
+
+console.log("Imam kucu na " + kuca.brojKatova + " spratu sa " + kuca.brojKvadrata + " kvadrata. Boja fasade je " + kuca.bojaFasade + ".");
