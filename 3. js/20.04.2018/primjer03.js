afae7362fc3089c585510bc7cfaaaf59a9643304
Edin Jašarević <edinjasar14@gmail.com>
2018-06-10 00:08:26 +0200

    postavljanje property index ime

---
+++ 3. js/20.04.2018/primjer03.js
@@ -0,0 +1,7 @@
+var osoba = {};
+
+osoba['ime'] = "Ime";
+osoba['prezime'] = "Prezime";
+osoba['brojGodina'] = 10;
+
+console.log("Ja sam " + osoba['ime'] + ", prezime mi je " + osoba['prezime'] + ". Tek mi je " + osoba['brojGodina'] + " godina.");
