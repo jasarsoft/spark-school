092e548ead9517f040ed9881c1a9da586df4ec6d
Edin Jašarević <edinjasar14@gmail.com>
2018-06-10 00:07:37 +0200

    objekat kuca

---
+++ 3. js/20.04.2018/primjer01.js
@@ -0,0 +1,7 @@
+var kuca = {
+	brojKatova: 3,
+  brojKvadrata: 75,
+  bojaFasade: "zuta"
+};
+
+console.log("Imam kucu na " + kuca.brojKatova + " spratu sa " + kuca.brojKvadrata + "kvadrata. Boja fasade je " + kuca.bojaFasade + ".");
